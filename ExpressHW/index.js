11fb69902cd367525e6e094eeb4852ffca0a6928
GLOBAL\I318110 <marius.ciuperca@sap.com>
2017-07-24 12:17:09 +0300

    Commit express2

--- ExpressHW/index.js
+++ ExpressHW/index.js
@@ -4,5 +4,14 @@ var app = express()
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
+
+app.post('/', function (req, res) {
+  res.send('Bye World!')
+})
+
+app.get('/hello/world', function (req,res){
+    res.send('Hello World 2');
+
+});
  
 app.listen(3000)
