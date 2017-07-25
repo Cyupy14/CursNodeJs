85a8ca31f34b320394a989a8d78cb2bbdea18d70
GLOBAL\I318110 <marius.ciuperca@sap.com>
2017-07-25 10:23:54 +0300

    commit exit

---
+++ Pexit/index.js
@@ -0,0 +1,21 @@
+process.on('exit', function (code) {
+
+    // Following code will never execute.
+    /*  setTimeout(function () {
+          console.log("This will not run");
+      }, 2000); */
+
+    console.log('About to exit with code:', code);
+});
+
+process.on('beforeExit', function (code) {
+
+    // Following code will never execute.
+      setTimeout(function () {
+          console.log("This will not run");
+      }, 0); 
+
+    console.log('About to exit with codeee:', code);
+});
+
+console.log("Program Ended");
